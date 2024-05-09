32d0bac5b17c35ed6229353df2f6bd6978721a57
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-05-09 20:08:40 +0530

    Add todoRoutes

---
+++ server/src/routes/todo.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { createTodo } from "../controllers/todo.controller.js";
+
+const router = Router();
+
+router.route("/create-todo").post(createTodo);
+
+export default router;
